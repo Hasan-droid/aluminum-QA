94fd64d2c1b18fc0b7e0089695f8f1e074d1d940
Abdallah Khasseb <khassibak47@gmail.com>
2025-11-27 15:24:23 +0300

    add a Mendix .py

--- test_automationWithAI.py
+++ test_automationWithAI.py
@@ -9,14 +9,14 @@ new_URL = None
 def test_login(al: Alumni, driver: Chrome):
     driver.get(f"http://172.16.2.56:3005/?redirect_uri=http%3A%2F%2F172.16.2.56%3A3002")
     driver.maximize_window()
-    time.sleep(4)
+    time.sleep(3.5)
     
    # al.do("Login with username = 'adminn' , password =  '123456789Aa@'")
     al.do("Type adminn into username field")
     al.do("Type 123456789Aa@ into password field")
     al.do("click sign in button")
     print("Done")
-    time.sleep(7)
+    time.sleep(6)
 
     global new_URL
     new_URL = driver.current_url
---
+++ test_of_mendix.py
@@ -0,0 +1,4 @@
+from cgitb import text
+from alumnium import Alumni
+from selenium.webdriver import Chrome
+import time
