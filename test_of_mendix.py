f4b4c0f2aae744fdc24fa8b18625a13d4e857be9
Abdallah Khasseb <khassibak47@gmail.com>
2025-12-01 09:57:06 +0300

    Add a starter to test a Mendix applicatrion (login for now)

--- test_of_mendix.py
+++ test_of_mendix.py
@@ -1,4 +1,13 @@
-from cgitb import text
 from alumnium import Alumni
 from selenium.webdriver import Chrome
 import time
+
+def test_login(al: Alumni, driver: Chrome):
+    driver.get(f"http://10.0.81.222:5050/index-rtl.html")
+    driver.maximize_window()
+    time.sleep(3.5)
+
+    al.do("Type 9951035944 into username field")
+    al.do(("Type P@ssw0rd into password field"))
+    al.do("click login button")
+    time.sleep(5)
