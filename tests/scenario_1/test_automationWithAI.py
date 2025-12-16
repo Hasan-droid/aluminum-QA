c0825156e46f285132b1b42121c72c0f01973279
abdallah khasseb <khassibak47@gmail.com>
2025-12-16 16:32:23 +0300

    Refactor test automation code to use class-based structure, consolidating state management for URL and cookies

--- tests/scenario_1/test_automationWithAI.py
+++ tests/scenario_1/test_automationWithAI.py
@@ -4,56 +4,51 @@ from selenium.webdriver import Chrome
 import json
 import time
 
-# Module-level variables to share state between tests
-new_URL = None
-cooke_json = None
-
 @pytest.mark.scenario1
-@pytest.mark.order(1)
-def test_login(al: Alumni, driver: Chrome):
-    driver.get(f"http://172.16.2.56:3005/?redirect_uri=http%3A%2F%2F172.16.2.56%3A3002")
-    driver.maximize_window()
-    time.sleep(3.5)
-    
-   # al.do("Login with username = 'adminn' , password =  '123456789Aa@'")
-    al.do("Type adminn into username field")
-    al.do("Type 123456789Aa@ into password field")
-    al.do("click sign in button")
-    print("Done")
-    time.sleep(6)
+class TestAutomationWithAI:
+    # Class-level variables to share state between tests
+    new_URL = None
+    cooke_json = None
 
-    global new_URL
-    new_URL = driver.current_url
-    
+    @pytest.mark.order(1)
+    def test_login(self, al: Alumni, driver: Chrome):
+        driver.get(rf"http://172.16.2.56:3005/?redirect_uri=http%3A%2F%2F172.16.2.56%3A3002")
+        driver.maximize_window()
+        time.sleep(3.5)
 
-@pytest.mark.scenario1
-@pytest.mark.order(2)
-def test_after_login(al: Alumni, driver: Chrome):
-    global new_URL
-    print(f"the new URL {new_URL}")
-    driver.get(new_URL)
-    
-    
-    al.do("hover the 'العربية' button on the navigation bar")
-    al.do("Click the 'العربية' button on the navigation bar")
+        # al.do("Login with username = 'adminn' , password =  '123456789Aa@'")
+        al.do("Type adminn into username field")
+        al.do("Type 123456789Aa@ into password field")
+        al.do("click sign in button")
+        print("Done")
+        time.sleep(6)
 
-    al.do("Click on 'تعريف الخدمات' card")
+        # store URL for subsequent tests in this class
+        TestAutomationWithAI.new_URL = driver.current_url
 
-    al.do("Click on 'الخدمات الفرعية' sub-tab")
-    time.sleep(3)
-    global cooke_json
-    cooke = driver.get_cookies()
-    cooke_json = json.dumps(cooke)
-    time.sleep(3)
-    
-@pytest.mark.scenario1
-@pytest.mark.order(3)
-def test_after_login2(al: Alumni, driver: Chrome):
-    global cooke_json
-    cookies = json.loads(cooke_json) if cooke_json else []
-    driver.get("http://172.16.2.56:3002/health")
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-    driver.get("http://172.16.2.56:3002/services/definition")
-    
-    al.do("Click on 'الخدمات الفرعية' sub-tab")
+    @pytest.mark.order(2)
+    def test_after_login(self, al: Alumni, driver: Chrome):
+        print(f"the new URL {TestAutomationWithAI.new_URL}")
+        driver.get(TestAutomationWithAI.new_URL)
+
+        al.do("hover the 'العربية' button on the navigation bar")
+        al.do("Click the 'العربية' button on the navigation bar")
+
+        al.do("Click on 'تعريف الخدمات' card")
+
+        al.do("Click on 'الخدمات الفرعية' sub-tab")
+        time.sleep(3)
+
+        cooke = driver.get_cookies()
+        TestAutomationWithAI.cooke_json = json.dumps(cooke)
+        time.sleep(3)
+
+    @pytest.mark.order(3)
+    def test_after_login2(self, al: Alumni, driver: Chrome):
+        cookies = json.loads(TestAutomationWithAI.cooke_json) if TestAutomationWithAI.cooke_json else []
+        driver.get("http://172.16.2.56:3002/health")
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+        driver.get("http://172.16.2.56:3002/services/definition")
+
+        al.do("Click on 'الخدمات الفرعية' sub-tab")
